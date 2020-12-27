9fc3cc77d26091a1d9c63b164143058b9192d416
MISTOM <kigardetom2001@gmail.com>
2020-12-27 15:45:59 +0300

    add glow effects

--- rps.js
+++ rps.js
@@ -3,29 +3,64 @@ let compScore= 0;
 const userScoreSpan= document.getElementById("userScore");
 const compScoreSpan= document.getElementById("compScore");
 const scoreBoardDiv= document.querySelector(".scoreBoard");
-const scoreMessage= document.getElementById("scoreResult");
+const scoreMessage= document.querySelector(".scoreResult");
 const rockDiv=document.getElementById("r");
 const paperDiv=document.getElementById("p");
 const scissorDiv=document.getElementById("s");
+const smUserWord = "User".fontsize(4).sup();
+const smCompWord = "Comp".fontsize(4).sup();
 
 function computerChoice() {
     const choices=['r', 'p', 's'];
     const randomNumber=Math.floor(Math.random() *3);
     return choices[randomNumber];
 }
+function convertToWord(letter){
+    if (letter === "r") return "rock";
+    if (letter === "p") return "paper";
+    if (letter === "s") return "scissor";
+}
 
+function win(userChoice, computerChoice){
+    userScore++
+    userScoreSpan.innerHTML = userScore;
+    compScoreSpan.innerHTML = compScore;
+    scoreMessage.innerHTML = `${smUserWord} ${convertToWord(userChoice)} beats ${smCompWord} ${convertToWord(computerChoice)}. YOU WIN 🔥`;
+    document.getElementById(userChoice).classList.add("green-glow");
+    setTimeout(function (){document.getElementById(userChoice).classList.remove("green-glow")}, 500);
+}
+function loose(userChoice, computerChoice){
+    compScore++
+    userScoreSpan.innerHTML = userScore;
+    compScoreSpan.innerHTML = compScore;
+    scoreMessage.innerHTML = `${smCompWord} ${convertToWord(computerChoice)} beats ${smUserWord} ${convertToWord(userChoice)}. YOU LOOSE !💩`;
+    document.getElementById(userChoice).classList.add("red-glow");
+    setTimeout(function (){document.getElementById(userChoice).classList.remove("red-glow")}, 500);
+}
+function draw(userChoice, computerChoice){
+    userScoreSpan.innerHTML = userScore;
+    compScoreSpan.innerHTML = compScore;
+    scoreMessage.innerHTML = `${smUserWord} ${convertToWord(userChoice)} draws to ${smCompWord} ${convertToWord(computerChoice)}. 🤷‍♂️ISSA DRAW🤷‍♀️`;
+    document.getElementById(userChoice).classList.add("gray-glow");
+    setTimeout(function (){document.getElementById(userChoice).classList.remove("gray-glow")}, 500);
+}
 function game(userChoice){
     const compChoice= computerChoice();
     switch (userChoice + compChoice) {
         case "rs":
         case "pr":
         case "sp":
-        console.log("userWins!");
+        win(userChoice, compChoice);
     break;
         case "sr":
         case "rp":
-        case "pr":
-        console.log(userLoose!);
+        case "ps":
+        loose(userChoice, compChoice);
+    break;
+        case "ss":
+        case "rr":
+        case "pp":
+        draw(userChoice, compChoice);
     break;
     }
 }
